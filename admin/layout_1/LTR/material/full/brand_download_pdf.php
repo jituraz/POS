d1ebd8beeb0c08d2219da45b1c97d1a05a96f49f
jituraz <jitoraz@gmail.com>
2023-06-01 16:19:29 +0600

    git ignor

--- admin/layout_1/LTR/material/full/brand_download_pdf.php
+++ admin/layout_1/LTR/material/full/brand_download_pdf.php
@@ -2,25 +2,11 @@
 include_once($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'config.php');
 $dataslide = file_get_contents($datasource . 'brand.json');
 $slides = json_decode($dataslide, "ture");
-// d($slides);
+// dd($slides);
 
 
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output();
+$slidesHTML =<<<SLIDE
 
-?>  
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
-</head>
-<body>
 <table border="1" width="100%" cellpadding="10" cellspacing="0">
   <thead>
     <th colspan="7" style="padding: 10px; font-size:larger">ALL BRAND LIST </th>
@@ -36,35 +22,44 @@ $mpdf->Output();
     </tr>
   </thead>
   <tbody style="text-align: center;">
-    <?php
+ 
+SLIDE;
+
+
+?>  
+<?php
+    $trs = null;
     foreach ($slides as $key => $slide) :
-    ?>
-      <!-- ...........1........... -->
+
+
+    
+     $tr =<<<TR
       <tr>
 
-        <td ><?= $slide['id'] ?></td>
+        <td ><?= $slide->id ?></td>
+
+
+        
         <td style="width: 80px;">
           <div class="d-flex align-items-center">
             <div class="col-md-2">
-              <a href="<?= $assets . $slide['src'] ?>" data-popup="lightbox">
-                <img src="<?= $assets . $slide['src'] ?>" width="100px" alt="" />
+              <a href="<?= $assets . $slide->src?>" data-popup="lightbox">
+                <img src="<?= $assets . $slide->src?>" width="100px" alt="" />
               </a>
             </div>
           </div>
         </td>
 
-        <td ><?= $slide['BrandName'] ?></td>
-        <td ><?= $slide['BrandCode'] ?></td>
-        <td ><?= $slide['Discription'] ?></td>
-        <td ><span class="badge badge-success"><?= $slide['Createdby'] ?></span></td>
+        <td ><?= $slide->BrandName?></td>
+        <td ><?= $slide->BrandCode?></td>
+        <td ><?= $slide->Discription?></td>
+        <td ><span class="badge badge-success"><?= $slide->Createdby?></span></td>
 
       </tr>
-    <?php 
+TR;
+echo $tr;
     endforeach
-     ?>
-
-    <!-- ...............0.............. -->
-  </tbody>
+?>
+     </tbody>
 </table>
-</body>
-</html>
+<!-- dd($slidesHTML); -->
